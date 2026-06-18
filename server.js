0290cd2ab17a367694ce7e6c0421837df74fba69
AdityaJoshiJ <adityajoshijj@gmail.com>
2026-06-19 03:16:51 +0530

    add CORS middleware support to backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -23,6 +24,7 @@ const app = express();
 })();
 
 // Init middleware
+app.use(cors());
 app.use(express.json({ extended: false }));
 
 // Define routes
